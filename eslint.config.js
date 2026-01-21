28b731af3ed6f589531e8f3d55c2d6a187893fd0
Vahid Ramezanipour <vahidramezanipour@gmail.com>
2026-01-21 12:58:58 +0100

    feat: fix config linting errors

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,14 @@
 // @ts-check
 
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
-import perfectionist from "eslint-plugin-perfectionist";
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import perfectionist from 'eslint-plugin-perfectionist';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import vitest from "@vitest/eslint-plugin";
-
-
+import vitest from '@vitest/eslint-plugin';
 
 export default [
   {
-    ignores: ["**/*.js"],
+    ignores: ['**/*.js'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.strictTypeChecked,
@@ -24,22 +22,25 @@ export default [
       },
     },
   },
-  perfectionist.configs["recommended-natural"],
+  perfectionist.configs['recommended-natural'],
   {
-    files: ["**/*.test.ts", "**/*.spec.ts"],
+    files: ['**/*.test.ts', '**/*.spec.ts'],
     plugins: {
       vitest,
     },
     rules: {
       ...vitest.configs.recommended.rules,
-      "@typescript-eslint/unbound-method": "off",
+      '@typescript-eslint/unbound-method': 'off',
     },
   },
   {
     rules: {
-      "@typescript-eslint/restrict-template-expressions": ["error", {
-        allowNumber: true
-      }]
-    }
-  }
+      '@typescript-eslint/restrict-template-expressions': [
+        'error',
+        {
+          allowNumber: true,
+        },
+      ],
+    },
+  },
 ];
