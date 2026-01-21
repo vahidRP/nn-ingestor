63d4e1ab78998914083cfefa913961681f7de6fd
Vahid Ramezanipour <vahidramezanipour@gmail.com>
2026-01-21 12:27:11 +0100

    feat: setup vitest

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,8 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import perfectionist from "eslint-plugin-perfectionist";
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import { rules } from "eslint-config-prettier";
+import vitest from "@vitest/eslint-plugin";
+
 
 
 export default [
@@ -24,6 +25,16 @@ export default [
     },
   },
   perfectionist.configs["recommended-natural"],
+  {
+    files: ["**/*.test.ts", "**/*.spec.ts"],
+    plugins: {
+      vitest,
+    },
+    rules: {
+      ...vitest.configs.recommended.rules,
+      "@typescript-eslint/unbound-method": "off",
+    },
+  },
   {
     rules: {
       "@typescript-eslint/restrict-template-expressions": ["error", {
